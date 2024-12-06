085009c3487ed5ce71a186ad20019e94710b1ae1
João Victor <joaovictorsalotialves12@gmail.com>
2024-12-06 15:00:46 -0300

    App

---
+++ src/application/use-cases/read-notification.spec.ts
@@ -0,0 +1,37 @@
+import { Content } from '@application/entities/content';
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notification-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+import { ReadNotification } from './read-notification';
+
+describe('Read notification', () => {
+  it('should be able to read a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const readNotification = new ReadNotification(notificationsRepository);
+
+    const notification = makeNotification({
+      category: 'social',
+      content: new Content('Nova solicitação de amizade!'),
+      recipientId: 'example-recipient-id',
+    });
+
+    await notificationsRepository.create(notification);
+
+    await readNotification.execute({ notificationId: notification.id });
+
+    expect(notificationsRepository.notifications[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to read a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const readNotification = new ReadNotification(notificationsRepository);
+
+    expect(() => {
+      return readNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
